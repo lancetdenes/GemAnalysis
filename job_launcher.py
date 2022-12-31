71d35edbab3e89aaeec226600f0b8f741aba409c
lancetdenes <lance.denes@nyulangone.org>
2022-12-30 20:30:41 -0500

    fixed paths, help statements, verbiosity

--- job_launcher.py
+++ job_launcher.py
@@ -8,15 +8,15 @@ import pandas as pd
 def launch_gem_job(nuc_file, model_path, gem_file):
     cmd = 'python segment_nuc_v2.py --nuc_file %s --modelpath %s\npython track_gems_v2.py --gem_file %s --nuc_file %s'%(nuc_file, model_path, gem_file, nuc_file)
     scriptOptions = {'jobname':'track_nuc_gems','time':'12:00:00','partition':'cpu_short','mem-per-cpu':'2gb', 'cpus-per-task':'4', 'gres':'0'}
-    Mysbatch_.launchJob(cmd,scriptOptions,verbose=True)
+    Mysbatch_.launchJob(cmd,scriptOptions)
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--test', '-t', action='store_true', help='testing run with file names provided directly')
+    parser.add_argument('--test', '-t', action='store_true', help='testing run with file names provided directly using -g and -n flags')
     parser.add_argument('--gem_file','-g', default=None, help='gem file to be processed')
     parser.add_argument('--nuc_file', '-n', default=None, help='nuc file to be processed')
-    parser.add_argument('--modelpath', '-m', default='/gpfs/scratch/denesl01/libLTD/CP_20221228_nuclei_60xNikonSD', help='path to cellpose model')
+    parser.add_argument('--modelpath', '-m', default='/gpfs/scratch/denesl01/libLTD/CP_20221228_nuclei_60xNikonSD', help='path to cellpose model, default model is CP_20221228_nuclei_60xNikonSD')
     parser.add_argument('--experiment', '-e', default=None, help='use this to run on a whole directory using a premade file list with gem_file and nuc_file columns')
     args=parser.parse_args()
 
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     else:
         scratch = os.environ['SCRATCH']
         experiment = args.experiment
-        file_list = os.path.join(scratch,experiment,'file_list.csv')
+        file_list = os.path.join(scratch,'projects','MSC_GEMs',experiment,'file_list.csv')
         if not os.path.exists(file_list):
             print('ERROR: file list not there!')
             exit()
@@ -37,5 +37,6 @@ if __name__ == '__main__':
         print('loaded file list!')
         model_path = args.modelpath
         print('model used is: %s'%model_path)
+        rawdata_path = os.path.join(scratch, 'projects', 'MSC_GEMs', experiment, 'rawdata')
         for n,line in file_list.iterrows():
-            launch_gem_job(line['nuc_file'], model_path, line['gem_file'])
+            launch_gem_job(os.path.join(rawdata_path, line['nuc_file']), model_path, os.path.join(rawdata_path, line['gem_file']))
